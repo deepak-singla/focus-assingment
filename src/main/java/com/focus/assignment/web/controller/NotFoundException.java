cc357599a64d2835e929f1ee89934dd487d90714
deepa <deepa@Deepak-Singla>
2022-12-13 22:21:36 +0530

    Added exceptions

---
+++ src/main/java/com/focus/assignment/web/controller/NotFoundException.java
@@ -0,0 +1,10 @@
+package com.focus.assignment.web.controller;
+
+
+public class NotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8306698953499650706L;
+}
